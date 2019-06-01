4be4f80846329ec8cb52b825d33e0030b32088a9
Unknown <david.chasseray@devinci.fr>
2019-06-02 01:20:51 +0200

    secure apiKey

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase';
 
 const app = initializeApp({
-  apiKey: 'AIzaSyDzXgk-cY7vtDKBkFt-yBwzSFyP4AX-ECQ',
+  apiKey: process.env.SECRET_KEY,
   authDomain: 'cat-mash-david.firebaseapp.com',
   databaseURL: 'https://cat-mash-david.firebaseio.com',
   projectId: 'cat-mash-david',
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App';
 import router from './router';
 import './firebase';
 
+require('dotenv').config();
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
