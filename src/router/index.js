5fdd0413d1fac504ed0aa0730cc1c89b98bea535
Unknown <david.chasseray@devinci.fr>
2019-06-01 15:35:12 +0200

    create needed routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Scores from '@/components/Scores';
+import Competition from '@/components/Competition';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
+      path: '/scores',
+      name: 'Scores',
+      component: Scores,
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Cats',
+      component: Competition,
     },
   ],
 });
